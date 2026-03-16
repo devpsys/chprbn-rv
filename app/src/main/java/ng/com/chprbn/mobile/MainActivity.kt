4fbf945f9369d9e69c07d9222f5c24619bd6fada
A. L. Zailani <devpsys@gmail.com>
2026-03-16 20:56:56 +0100

    update icon/logo

--- app/src/main/java/ng/com/chprbn/mobile/MainActivity.kt
+++ app/src/main/java/ng/com/chprbn/mobile/MainActivity.kt
@@ -71,19 +71,3 @@ private fun DesignSystemPreviewScreen() {
         StatusBadge(status = Status.Expired)
     }
 }
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CHPRBNTheme {
-        Greeting("Android")
-    }
-}
